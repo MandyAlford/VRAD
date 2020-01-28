ed74589b993ac94028615c8e01e3eca71d9c2026
Eric Weissman <eric.weissman21@gmail.com>
2020-01-28 11:11:58 -0700

    Adjust data for listing routes

--- areaDetails.js
+++ areaDetails.js
@@ -7,7 +7,14 @@ module.exports = {
       about: "RiNo is a burgeoning area with new bars, restaurants and event spaces popping up all the time. Explore this thriving area of Denver today!",
       region_code: 6356834,
       quick_search: "o5kod9f5cqo0",
-      listings: []
+      listings: [
+        '/api/v1/listings/3',
+        '/api/v1/listings/44',
+        '/api/v1/listings/221',
+        '/api/v1/listings/744',
+        '/api/v1/listings/90',
+        '/api/v1/listings/310',
+      ]
     },
     {
       id: 751,
@@ -16,7 +23,11 @@ module.exports = {
       about: "Park Hill features one of the best views of the downtown area and surrounding mountains. With easy access to City Park and the major highways, Park Hill also includes many unique styles of homes.",
       region_code: 6648386,
       quick_search: "g1m0tsxzl0o0",
-      listings: []
+      listings: [
+        '/api/v1/listings/3921',
+        '/api/v1/listings/56',
+        '/api/v1/listings/21',
+      ]
     },
     {
       id: 408,
@@ -25,7 +36,15 @@ module.exports = {
       about: "The Lower Highlands area, often referred to as the Highlands or LoHi, was one of the first areas to experience massive growth from the downtown area. Known for many great bars and restaurants with a great eastern facing view of the Downtown area.",
       region_code: 640399,
       quick_search: "17klwudb1h340",
-      listings: []
+      listings: [
+        '/api/v1/listings/83331',
+        '/api/v1/listings/411',
+        '/api/v1/listings/92',
+        '/api/v1/listings/6135',
+        '/api/v1/listings/9',
+        '/api/v1/listings/11',
+        '/api/v1/listings/77'
+      ]
     },
     {
       id: 240,
@@ -34,7 +53,12 @@ module.exports = {
       about: "Capitol Hill is home to many historic homes and parks in the Denver area. Cap Hill offers great proximity to Downtown area and has many options for enjoying the wonderful weather in Denver.",
       region_code: 6035251,
       quick_search: "2mxzlp185o800",
-      listings: []
+      listings: [
+        '/api/v1/listings/66',
+        '/api/v1/listings/555',
+        '/api/v1/listings/27',
+        '/api/v1/listings/8',
+      ]
     }
   ]
 }
