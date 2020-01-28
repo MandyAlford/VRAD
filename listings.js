650d843b2f91543737e97c0e1b3dd6ae3d2302be
Eric Weissman <eric.weissman21@gmail.com>
2020-01-28 09:33:13 -0700

    Add CORS

---
+++ listings.js
@@ -0,0 +1,486 @@
+module.exports = {
+  listings: [
+    {
+      photo_id: 3,
+      name: "Hip RiNo Party Spot",
+      address: {
+        street: "2250 Lawrence St",
+        zip: "80205"
+      },
+      details: {
+        neighborhood_id: 5124122,
+        superhost: true,
+        seller_source: "91jss1",
+        beds: 3,
+        baths: 2.5,
+        cost_per_night: 420,
+        features: [
+          "hot tub",
+          "espresso machine"
+        ],
+      },
+      dev_id: "u4gh2j",
+      area: "rino",
+      db_connect: 834470
+    },
+    {
+      photo_id: 44,
+      name: "Lowkey Industrial Chic",
+      address: {
+        street: "2441 Broadway Ave",
+        zip: "80205"
+      },
+      details: {
+        neighborhood_id: 5124122,
+        superhost: true,
+        seller_source: "91jss1",
+        beds: 1,
+        baths: 1.5,
+        cost_per_night: 220,
+        features: [
+          "city views",
+          "industrial motif",
+          "rooftop"
+        ]
+      },
+      dev_id: "jaenku",
+      area: "rino",
+      db_connect: 694530
+    },
+    {
+      photo_id: 221,
+      name: "New Modern Flat in RiNo",
+      address: {
+        street: "2459 Lawrence St",
+        zip: "80205"
+      },
+      details: {
+        neighborhood_id: 5124122,
+        superhost: true,
+        seller_source: "91jss1",
+        beds: 3,
+        baths: 5,
+        cost_per_night: 285,
+        features: [
+          "rooftop",
+          "proximity to bars/restaurants",
+          "new appliances"
+        ]
+      },
+      dev_id: "amukct",
+      area: "rino",
+      db_connect: 872937
+    },
+    {
+      photo_id: 744,
+      name: "Upscale Modern Apartments",
+      address: {
+        street: "2345 Walnut St",
+        zip: 80205
+      },
+      details: {
+        neighborhood_id: 5124122,
+        superhost: false,
+        seller_source: "91jss1",
+        beds: 2,
+        baths: 2.5,
+        cost_per_night: 299,
+        features: [
+          "workout facility",
+          "fireplace",
+          "patio with grill"
+        ]
+      },
+      dev_id: "k6h524",
+      area: "rino",
+      db_connect: 233658
+    },
+    {
+      photo_id: 90,
+      name: "Loft Living in RiNo",
+      address: {
+        street: "2245 Blake St",
+        zip: 80205
+      },
+      details: {
+        neighborhood_id: 5124122,
+        superhost: true,
+        seller_source: "91jss1",
+        beds: 2,
+        baths: 3,
+        cost_per_night: 400,
+        features: [
+          "modern loft design",
+          "natural light",
+          "downtown views"
+        ]
+      },
+      dev_id: "lskn6m",
+      area: "rino",
+      db_connect: 180124
+    },
+    {
+      photo_id: 310,
+      name: "Brand New RiNo Build",
+      address: {
+        street: "2525 Arapahoe St",
+        zip: 80205
+      },
+      details: {
+        neighborhood_id: 5124122,
+        superhost: true,
+        seller_source: "91jss1",
+        beds: 1,
+        baths: .75,
+        cost_per_night: 175,
+        features: []
+      },
+      dev_id: "vwv1vj",
+      area: "rino",
+      db_connect: 173411
+    },
+    {
+      photo_id: 3921,
+      name: "Spacious New Build in Park Hill",
+      address: {
+        street: "2127 Clay St",
+        zip: 80207
+      },
+      details: {
+        neighborhood_id: 018512,
+        superhost: false,
+        seller_source: "kksa722",
+        beds: 2,
+        baths: 3,
+        cost_per_night: 185,
+        features: [
+          "spacious yard",
+          "outdoor patio",
+          "cool neighborhood"
+        ]
+      },
+      dev_id: "twxpxc",
+      area: "parkHill",
+      db_connect: 989167
+    },
+    {
+      photo_id: 56,
+      name: "Updated Park Hill Duplex",
+      address: {
+        street: "935 S Clarkson St",
+        zip: 80209
+      },
+      details: {
+        neighborhood_id: 018512,
+        superhost: true,
+        seller_source: "kksa722",
+        beds: 2,
+        baths: 1,
+        cost_per_night: 165,
+        features: [
+          "close to Wash Park",
+          "cozy front porch"
+        ]
+      },
+      dev_id: "3bx72v",
+      area: "parkHill",
+      db_connect: 117825
+    },
+    {
+      photo_id: 21,
+      name: "Historic Park Hill abode",
+      address: {
+        street: "135 N Logan St",
+        zip: 80203
+      },
+      details: {
+        neighborhood_id: 018512,
+        superhost: true,
+        seller_source: "kksa722",
+        beds: 4,
+        baths: 3,
+        cost_per_night: 395,
+        features: [
+          "spacious front yard",
+          "shaded backyard"
+        ]
+      },
+      dev_id: "5c45kq",
+      area: "parkHill",
+      db_connect: 169591
+    },
+    {
+      photo_id: 83331,
+      name: "LoHi Heaven",
+      address: {
+        street: "3607 Navajo St",
+        zip: 80211
+      },
+      details: {
+        neighborhood_id: 99125,
+        superhost: false,
+        seller_source: "kfbr392",
+        beds: 3,
+        baths: 2.75,
+        cost_per_night: 525,
+        features: [
+          "elegant living space",
+          "fireplace",
+          "rooftop",
+          "modern bathrooms",
+          "proximity to bars/restaurants"
+        ]
+      },
+      dev_id: "27gu74",
+      area: "lohi",
+      db_connect: 489974
+    },
+    {
+      photo_id: 411,
+      name: "LoHi Bungalow",
+      address: {
+        street: "3349 Zuni St",
+        zip: 80211
+      },
+      details: {
+        neighborhood_id: 99125,
+        superhost: false,
+        seller_source: "kfbr392",
+        beds: 5,
+        baths: 3,
+        cost_per_night: 515,
+        features: [
+          "backyard space",
+          "fireplace",
+          "exposed brick",
+          "unique layout"
+        ]
+      },
+      dev_id: "c78t9d",
+      area: "lohi",
+      db_connect: 989089
+    },
+    {
+      photo_id: 92,
+      name: "Quaint Lohi Duplex",
+      address: {
+        street: "1822 W 35th Ave",
+        zip: 80211
+      },
+      details: {
+        neighborhood_id: 99125,
+        superhost: true,
+        seller_source: "kfbr392",
+        beds: 3,
+        baths: 2,
+        cost_per_night: 399,
+        features: [
+          "front porch",
+          "backyard area",
+          "historic layout",
+          "proximity to bars/restaurants"
+        ]
+      },
+      dev_id: "m8382j",
+      area: "lohi",
+      db_connect: 807504
+    },
+    {
+      photo_id: 6135,
+      name: "Downtown Views Galore",
+      address: {
+        street: "1750 Wewatta St",
+        zip: 80202
+      },
+      details: {
+        neighborhood_id: 99125,
+        superhost: true,
+        seller_source: "kfbr392",
+        beds: 1,
+        baths: 1.5,
+        cost_per_night: 225,
+        features: [
+          "downtown views",
+          "rooftop",
+          "pool",
+          "fireplace",
+          "updated appliances"
+        ]
+      },
+      dev_id: "9ip81y",
+      area: "lohi",
+      db_connect: 468486
+    },
+    {
+      photo_id: 9,
+      name: "LoHi Super Pad",
+      address: {
+        street: "3731 Lipan St",
+        zip: 80211
+      },
+      details: {
+        neighborhood_id: 99125,
+        superhost: false,
+        seller_source: "kfbr392",
+        beds: 4,
+        baths: 3.5,
+        cost_per_night: 600,
+        features: [
+          "updated kitchen",
+          "fireplace",
+          "rooftop",
+          "wetbar",
+          "backyard area"
+        ]
+      },
+      dev_id: "2f2pok",
+      area: "lohi",
+      db_connect: 154813
+    },
+    {
+      photo_id: 11,
+      name: "Gorgeous LoHi Home",
+      address: {
+        street: "2817 W 33rd Ave",
+        zip: 80211
+      },
+      details: {
+        neighborhood_id: 99125,
+        superhost: true,
+        seller_source: "kfbr392",
+        beds: 3,
+        baths: 3,
+        cost_per_night: 545,
+        features: [
+          "frontyard area",
+          "huge master bedroom",
+          "natural light",
+          "backyard area",
+          "unique fixtures and trimmings"
+        ]
+      },
+      dev_id: "v6ye08",
+      area: "lohi",
+      db_connect: 842055
+    },
+    {
+      photo_id: 77,
+      name: "Open Layout LoHi Getaway",
+      address: {
+        street: "1700 Bassett St",
+        zip: 80202
+      },
+      details: {
+        neighborhood_id: 99125,
+        superhost: true,
+        seller_source: "kfbr392",
+        beds: 1,
+        baths: 1,
+        cost_per_night: 285,
+        features: [
+          "open design",
+          "natural light",
+          "hardwood floors"
+        ]
+      },
+      dev_id: "vlb9tn",
+      area: "lohi",
+      db_connect: 247208
+    },
+    {
+      photo_id: 66,
+      name: "Cap Hill Overlook",
+      address: {
+        street: "925 N Lincoln St",
+        zip: 80203
+      },
+      details: {
+        neighborhood_id: 651251,
+        superhost: true,
+        seller_source: "55ccsa",
+        beds: 3,
+        baths: 1.75,
+        cost_per_night: 400,
+        features: [
+          "rooftop",
+          "fireplace",
+          "pool",
+          "leather couch"
+        ]
+      },
+      dev_id: "rcirdo",
+      area: "capHill",
+      db_connect: 736524
+    },
+    {
+      photo_id: 555,
+      name: "Cap Hill Duplex Delight",
+      address: {
+        street: "430 E 6th Ave",
+        zip: 80203
+      },
+      details: {
+        neighborhood_id: 651251,
+        superhost: false,
+        seller_source: "55ccsa",
+        beds: 3,
+        baths: 1,
+        cost_per_night: 250,
+        features: []
+      },
+      dev_id: "nqtmmj",
+      area: "capHill",
+      db_connect: 681159
+    },
+    {
+      photo_id: 27,
+      name: "Cap Hill Castle",
+      address: {
+        street: "1424 N Clarkson St",
+        zip: 80218
+      },
+      details: {
+        neighborhood_id: 651251,
+        superhost: true,
+        seller_source: "55ccsa",
+        beds: 3,
+        baths: 3,
+        cost_per_night: 445,
+        features: [
+          "front porch area",
+          "vintage/historic design and layout",
+        ]
+      },
+      dev_id: "q9bl3w",
+      area: "capHill",
+      db_connect: 182213
+    },
+    {
+      photo_id: 8,
+      name: "Elegant Cap Hill Mansion",
+      address: {
+        street: "1070 N Humboldt St",
+        zip: 80218
+      },
+      details: {
+        neighborhood_id: 651251,
+        superhost: false,
+        seller_source: "55ccsa",
+        beds: 8,
+        baths: 6,
+        cost_per_night: 1275,
+        features: [
+          "historic mansion",
+          "definitely no ghosts",
+          "can have Clue-themed dinner parties",
+          "creepy artwork",
+          "more stained glass than you can handle",
+          "beautiful backyard"
+        ]
+      },
+      dev_id: "rcirdo",
+      area: "capHill",
+      db_connect: 736524
+    }
+  ]
+}
--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const { areas } = require('./areas');
+const { listings } = require('./listings');
 
+app.use(cors());
 app.set('port', process.env.PORT || 3000);
+
+app.locals = {
+  title: 'Listings for Denver',
+  listings,
+  areas
+}
 app.locals.title = 'Listings for Denver Area';
 
 app.get('/', (request, response) => {
@@ -10,4 +20,11 @@ app.get('/', (request, response) => {
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`);
+});
+
+
+app.get('/api/v1/listings', (request, response) => {
+  const listings = app.locals.listings;
+
+  response.json({ listings });
 });
